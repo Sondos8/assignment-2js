4ed6641da3b7065788cb61da8055007d50d38c84
sondos <sondosmohamedee092@gmail.com>
2023-12-11 16:37:00 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const arrayOfQuotes = [
+    {'author': 'Jim Rohn', 
+     'quote': 'Beware of what you become in pursuit of what you want.'
+    },
+    {'author': 'Epictetus', 
+     'quote': 'It\'s not what happens to you, but how you react to it that matters.'
+    },
+    {'author': 'Frank Sinatra', 
+     'quote': 'The best revenge is massive success.'
+    },
+    {'author': 'Wayne Gretzy', 
+     'quote': 'You miss 100% of the shots you don\'t take.'
+    },
+    {'author': 'Frank Zappa', 
+     'quote': '“So many books, so little time.”'
+    },
+    {'author': 'Marcus Tullius Cicero', 
+     'quote': '“A room without books is like a body without a soul.”'
+    },
+];
+
+function generateQuote(){
+    const random = Number.parseInt(Math.random()*arrayOfQuotes.length + 1);
+    document.querySelector('#quoteOutput').textContent = `\"${arrayOfQuotes[random].quote}\"`;
+    document.querySelector('#authorOutput').textContent = `--${arrayOfQuotes[random].author}`;
+}
